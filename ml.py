6a676b6895c04df9f85549b7b39f2698bde5b949
Shema Leandre <iamshemaleandre@gmail.com>
2025-09-17 10:41:37 +0200

    change list items

--- ml.py
+++ ml.py
@@ -1,5 +1,8 @@
 
 thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+thislist[1:2] = "blackcurrant"
 
-print("Reversed :", thislist[::-1]) # ['apple', 'mango', 'cherry']
+print(thislist)
+
 
