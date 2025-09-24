13d6ab776672ce3cf19dc3dbde0dfdbd984831a2
Shema Leandre <iamshemaleandre@gmail.com>
2025-09-24 12:08:58 +0200

    compiling

--- ml.py
+++ ml.py
@@ -1,10 +1,9 @@
-def simple_function(x, y=None):
-    if y is None:
-        y = []
+def simple_function(x, y=[0]):
+    y.append(x)
     return y
 result1 = simple_function(1)
-result2 = simple_function(2, [])
-result3 = simple_function(3)
+result2 = simple_function(2, [3])
+result3 = simple_function(4)
 
 print(result1)
 print(result2)
