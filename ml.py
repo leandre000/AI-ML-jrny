fbcd82bf54c9970a49e8f8d461057cd393839af9
Shema Leandre <iamshemaleandre@gmail.com>
2025-09-24 12:04:34 +0200

    function behaviour

--- ml.py
+++ ml.py
@@ -77,3 +77,15 @@
 # print(list_length(my_list))
 
 
+def simple_function(x, y=None):
+    if y is None:
+        y = []
+    return y
+
+result1 = simple_function(1)
+result2 = simple_function(2, [])
+result3 = simple_function(3)
+
+print(result1)
+print(result2)
+print(result3)
