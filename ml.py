6bf3a32c1b93201953ec15d55c4ff98d3e701997
Shema Leandre <iamshemaleandre@gmail.com>
2025-10-08 11:41:34 +0200

    testing the function

--- ml.py
+++ ml.py
@@ -65,15 +65,27 @@
 # result2 = add(5)
 # print(result2)
 
-def simple_function(x, y=None):
-    if y is None:
-        y = []
+# def simple_function(x, y=None):
+#     if y is None:
+#         y = []
+#     y.append(x)
+#     return y
+#
+# result1 = simple_function(1)
+# result2 = simple_function(2, [])
+# result3 = simple_function(3)
+#
+# print(result1)
+# print(result2)
+# print(result3)
+
+def simple_function(x, y=[0]):
     y.append(x)
     return y
 
 result1 = simple_function(1)
-result2 = simple_function(2, [])
-result3 = simple_function(3)
+result2 = simple_function(2, [3])
+result3 = simple_function(4)
 
 print(result1)
 print(result2)
