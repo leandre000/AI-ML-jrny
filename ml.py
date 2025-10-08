eb2fdf651ba1d296b9eeb443ba15cd8c4c47cc81
Shema Leandre <iamshemaleandre@gmail.com>
2025-10-08 11:46:44 +0200

    changed value of y

--- ml.py
+++ ml.py
@@ -83,10 +83,10 @@ def simple_function(x, y=[0]):
     y.append(x)
     return y
 
-result1 = simple_function(1)
-result2 = simple_function(2, [3])
+# result1 = simple_function(1)
+# result2 = simple_function(2, [3])
 result3 = simple_function(4)
 
-print(result1)
-print(result2)
+# print(result1)
+# print(result2)
 print(result3)
